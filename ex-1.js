4217d7d0624fcba81a12d4faf443be1e7077457c
Supakit <book0970403815@hotmail.com>
2023-10-07 23:54:11 +0700

    fix : send assignment by book

--- ex-1.js
+++ ex-1.js
@@ -1,12 +1,18 @@
 // Exercise #1: For Each Function
 
-function forEach(array, operation) {
-  // Start coding here
-}
+function forEach(array,operation) {
+    newEmployeeSalaries.push(operation(array))
+    }
 
+  function addSalary(array){
+    for(let i in array){
+       array[i] += 5000
+       console.log(array[i])
+     }
+     return array
+    }
 const employeeSalaries = [20005, 40000, 32000, 14500, 344000];
 const newEmployeeSalaries = [];
 
-// Using `forEach` function here
-
+forEach(employeeSalaries,addSalary)
 console.log(newEmployeeSalaries); // [25005, 45000, 37000, 19500, 349000]
--- ex-2.js
+++ ex-2.js
@@ -1,15 +1,25 @@
 //Exercise #2: At Least Five Function
-
 function atLeastFive(array, operation) {
-  // Start coding here
+  console.log(operation(array))
 }
 
 const studentScoresRoom1 = [90, 40, 67, 80, 100, 15, 86, 12, 99, 67];
 const studentScoresRoom2 = [78, 98, 23, 15, 40, 12, 40, 67, 80, 100];
 const studentScoresRoom3 = [67, 80, 100, 15, 12, 40, 67, 80, 100, 67];
 
-// Using `atLeastFive` function here
+function operation(array){
+  let count = 0
+   for(let i in array){
+   if(array[i] > 70){
+    count += 1};
+  }
+  return count >= 5 
+  }
 
-let scoreRoom1Result;
-let scoreRoom2Result;
-let scoreRoom3Result;
+  atLeastFive(studentScoresRoom1,operation)
+  atLeastFive(studentScoresRoom2,operation)
+  atLeastFive(studentScoresRoom3,operation)
+
+let scoreRoom1Result = true;
+let scoreRoom2Result = false;
+let scoreRoom3Result = false;
